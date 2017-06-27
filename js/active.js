8e98aad9af33ac444a21af895beba62e092a0832
migmira <migmira@hotmail.com>
2017-06-27 04:12:24 +0000

    temp fix

--- js/active.js
+++ js/active.js
@@ -2,83 +2,83 @@
 	var path = window.location.pathname;
 	console.log(path);
 
-    $(document).on('DOMNodeInserted', function(e) {
+ //   $(document).on('DOMNodeInserted', function(e) {
 
-         console.log(e);
+ //        console.log(e);
    
-		switch( path ) {
-			case '/adjudication-core.html':
-				$("#whoweare").addClass('active');
+	// 	switch( path ) {
+	// 		case '/adjudication-core.html':
+	// 			$("#whoweare").addClass('active');
 				
-			case '/organizing-comittee.html':
-				$("#whoweare").addClass('active');
+	// 		case '/organizing-comittee.html':
+	// 			$("#whoweare").addClass('active');
 
-			case '/tab-team.html':
-				$("#whoweare").addClass('active');
+	// 		case '/tab-team.html':
+	// 			$("#whoweare").addClass('active');
 			
-			case '/wudc.html':
-				$("#about").addClass('active');
+	// 		case '/wudc.html':
+	// 			$("#about").addClass('active');
 				
-			case '/hosts.html':
-				$("#about").addClass('active');
+	// 		case '/hosts.html':
+	// 			$("#about").addClass('active');
 				
-			case '/mexicocity.html':
-				$("#about").addClass('active');
+	// 		case '/mexicocity.html':
+	// 			$("#about").addClass('active');
 				
-			case '/registration.html':
-				$("#tournament").addClass('active');
+	// 		case '/registration.html':
+	// 			$("#tournament").addClass('active');
 				
-			case '/payment.html':
-				$("#tournament").addClass('active');
+	// 		case '/payment.html':
+	// 			$("#tournament").addClass('active');
 				
-			case '/schedule.html':
-				$("#tournament").addClass('active');
+	// 		case '/schedule.html':
+	// 			$("#tournament").addClass('active');
 				
-			case '/independent-adjudicators.html':
-				$("#tournament").addClass('active');
+	// 		case '/independent-adjudicators.html':
+	// 			$("#tournament").addClass('active');
 				
-			case '/masters-public.html':
-				$("#tournament").addClass('active');
+	// 		case '/masters-public.html':
+	// 			$("#tournament").addClass('active');
 				
-			case '/code-conduct.html':
-				$("#tournament").addClass('active');
+	// 		case '/code-conduct.html':
+	// 			$("#tournament").addClass('active');
 				
-			case '/venues.html':
-				$("#tournament").addClass('active');
+	// 		case '/venues.html':
+	// 			$("#tournament").addClass('active');
 				
-			case '/visas.html':
-				$("#tournament").addClass('active');
+	// 		case '/visas.html':
+	// 			$("#tournament").addClass('active');
 				
-			case '/accomodation.html':
-				$("#tournament").addClass('active');
+	// 		case '/accomodation.html':
+	// 			$("#tournament").addClass('active');
 				
-			case '/volunteers.html':
-				$("#tournament").addClass('active');
+	// 		case '/volunteers.html':
+	// 			$("#tournament").addClass('active');
 				
-			case '/training-materials.html':
-				$("#tournament").addClass('active');
+	// 		case '/training-materials.html':
+	// 			$("#tournament").addClass('active');
 				
-			case '/faqs.html':
-				$("#tournament").addClass('active');	
+	// 		case '/faqs.html':
+	// 			$("#tournament").addClass('active');	
 
-			case '/press-releases.html':
-				$("#partners").addClass('active');
+	// 		case '/press-releases.html':
+	// 			$("#partners").addClass('active');
 				
-			case '/articles-interviewss.html':
-				$("#partners").addClass('active');
+	// 		case '/articles-interviewss.html':
+	// 			$("#partners").addClass('active');
 				
-			case '/gallery.html':
-				$("#partners").addClass('active');
+	// 		case '/gallery.html':
+	// 			$("#partners").addClass('active');
 
-			case '/contact.html':
-				$("#contact").addClass('active');		
+	// 		case '/contact.html':
+	// 			$("#contact").addClass('active');		
 				
-			default:
-				break;
-		}
+	// 		default:
+	// 			break;
+	// 	}
 
 
 
-	});
+	// });
 	
 // });
