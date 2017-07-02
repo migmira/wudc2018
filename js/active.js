6129363fe7e0452dbc1065d98ddb26f401f3d312
Miguel Windows <migmira@hotmail.com>
2017-07-02 12:39:01 -0500

    fixing active 2

--- js/active.js
+++ js/active.js
@@ -8,100 +8,100 @@
    
 		switch( path ) {
 
-			case '/index.html' || '/es/index.html':
+			case ( '/' || '/index.html' || '/es/' || '/es/index.html' ) :
 				$("#home").addClass('active');
 				break;
 
-			case '/adjudication-core.html' || '/es/adjudicacion.html' :
+			case ( '/adjudication-core.html' || '/es/adjudicacion.html'  ) :
 				$("#whoweare").addClass('active');
 				break;
 				
-			case '/organizing-comittee.html' || '/es/comiteorganizador.html':
+			case ( '/organizing-comittee.html' || '/es/comiteorganizador.html' ) :
 				$("#whoweare").addClass('active');
 				break;
 
-			case '/tab-team.html':
+			case ( '/tab-team.html' ) :
 				$("#whoweare").addClass('active');
 				break;
 			
-			case '/wudc.html' || '/es/campeonato.html':
+			case ( '/wudc.html' || '/es/campeonato.html' ) :
 				$("#about").addClass('active');
 				console.log('entro');
 				break;
 				
-			case '/hosts.html' || 'es/anfitriones.html' :
+			case ( '/hosts.html' || 'es/anfitriones.html'  ) :
 				$("#about").addClass('active');
 				break;
 				
-			case '/mexicocity.html' || '/es/cdmx.html':
+			case ( '/mexicocity.html' || '/es/cdmx.html' ) :
 				$("#about").addClass('active');
 				break;
 				
-			case '/registration.html' || '/es/registro.html':
+			case ( '/registration.html' || '/es/registro.html' ) :
 				$("#tournament").addClass('active');
 				break;
 				
-			case '/payment.html':
+			case ( '/payment.html' ) :
 				$("#tournament").addClass('active');
 				break;
 				
-			case '/schedule.html':
+			case ( '/schedule.html' ) :
 				$("#tournament").addClass('active');
 				break;
 				
-			case '/independent-adjudicators.html':
+			case ( '/independent-adjudicators.html' ) :
 				$("#tournament").addClass('active');
 				break;
 				
-			case '/masters-public.html':
+			case ( '/masters-public.html' ) :
 				$("#tournament").addClass('active');
 				break;
 				
-			case '/code-conduct.html':
+			case ( '/code-conduct.html' ) :
 				$("#tournament").addClass('active');
 				break;
 				
-			case '/venues.html':
+			case ( '/venues.html' ) :
 				$("#tournament").addClass('active');
 				break;
 				
-			case '/visas.html':
+			case ( '/visas.html' ) :
 				$("#tournament").addClass('active');
 				break;
 				
-			case '/accomodation.html':
+			case ( '/accomodation.html' ) :
 				$("#tournament").addClass('active');
 				break;
 				
-			case '/volunteers.html':
+			case ( '/volunteers.html' ) :
 				$("#tournament").addClass('active');
 				break;
 				
-			case '/training-materials.html':
+			case ( '/training-materials.html' ) :
 				$("#tournament").addClass('active');
 				break;
 				
-			case '/faqs.html':
+			case ( '/faqs.html' ) :
 				$("#tournament").addClass('active');
 				break;
 
-			case '/press-releases.html' || '/es/comunicados-prensa.html':
+			case ( '/press-releases.html' || '/es/comunicados-prensa.html' ) :
 				$("#partners").addClass('active');
 				break;
 				
-			case '/articles-interviewss.html':
+			case ( '/articles-interviews.html' || '/es/articulos-entrevistas.html' ) :
 				$("#partners").addClass('active');
 				break;
 				
-			case '/gallery.html' || '/es/galeria.html':
+			case ( '/gallery.html' || '/es/galeria.html' ) :
 				$("#partners").addClass('active');
 				break;
 
-			case '/contact.html' || '/es/contacto.html':
+			case ( '/contact.html' || '/es/contacto.html' ) :
 				$("#contact").addClass('active');	
 				break;
 				
-			default:
+			default  :
 				break;
 		}
 
