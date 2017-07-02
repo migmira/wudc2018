bb87ee517457656f79623903101cc5dc65839365
Miguel Windows <migmira@hotmail.com>
2017-07-02 13:35:32 -0500

    fixing active 3

--- js/active.js
+++ js/active.js
@@ -4,107 +4,119 @@
 
     $(document).on('DOMNodeInserted', function(e) {
 
-        // console.log(e);
-   
-		switch( path ) {
+        console.log(e);
 
-			case ( '/' || '/index.html' || '/es/' || '/es/index.html' ) :
-				$("#home").addClass('active');
-				break;
 
-			case ( '/adjudication-core.html' || '/es/adjudicacion.html'  ) :
-				$("#whoweare").addClass('active');
-				break;
-				
-			case ( '/organizing-comittee.html' || '/es/comiteorganizador.html' ) :
-				$("#whoweare").addClass('active');
-				break;
+        switch( path ) {
 
-			case ( '/tab-team.html' ) :
-				$("#whoweare").addClass('active');
-				break;
-			
-			case ( '/wudc.html' || '/es/campeonato.html' ) :
-				$("#about").addClass('active');
-				console.log('entro');
-				break;
-				
-			case ( '/hosts.html' || 'es/anfitriones.html'  ) :
-				$("#about").addClass('active');
-				break;
-				
-			case ( '/mexicocity.html' || '/es/cdmx.html' ) :
-				$("#about").addClass('active');
-				break;
-				
-			case ( '/registration.html' || '/es/registro.html' ) :
-				$("#tournament").addClass('active');
-				break;
-				
-			case ( '/payment.html' ) :
-				$("#tournament").addClass('active');
-				break;
-				
-			case ( '/schedule.html' ) :
-				$("#tournament").addClass('active');
-				break;
-				
-			case ( '/independent-adjudicators.html' ) :
-				$("#tournament").addClass('active');
-				break;
-				
-			case ( '/masters-public.html' ) :
-				$("#tournament").addClass('active');
-				break;
-				
-			case ( '/code-conduct.html' ) :
-				$("#tournament").addClass('active');
-				break;
-				
-			case ( '/venues.html' ) :
-				$("#tournament").addClass('active');
-				break;
-				
-			case ( '/visas.html' ) :
-				$("#tournament").addClass('active');
-				break;
-				
-			case ( '/accomodation.html' ) :
-				$("#tournament").addClass('active');
-				break;
-				
-			case ( '/volunteers.html' ) :
-				$("#tournament").addClass('active');
-				break;
-				
-			case ( '/training-materials.html' ) :
-				$("#tournament").addClass('active');
-				break;
-				
-			case ( '/faqs.html' ) :
-				$("#tournament").addClass('active');
-				break;
+            case '/':
+            case '/index.html':
+            case '/es/':
+            case '/es/index.html':
+                $("#home").addClass('active');
+                break;
 
-			case ( '/press-releases.html' || '/es/comunicados-prensa.html' ) :
-				$("#partners").addClass('active');
-				break;
-				
-			case ( '/articles-interviews.html' || '/es/articulos-entrevistas.html' ) :
-				$("#partners").addClass('active');
-				break;
-				
-			case ( '/gallery.html' || '/es/galeria.html' ) :
-				$("#partners").addClass('active');
-				break;
+            case '/adjudication-core.html' :
+            case '/es/adjudicacion.html':
+                $("#whoweare").addClass('active');
+                break;
 
-			case ( '/contact.html' || '/es/contacto.html' ) :
-				$("#contact").addClass('active');	
-				break;
-				
-			default  :
-				break;
-		}
+            case  '/organizing-comittee.html':
+            case '/es/comiteorganizador.html':
+                $("#whoweare").addClass('active');
+                break;
 
+            case ( '/tab-team.html' ) :
+                $("#whoweare").addClass('active');
+                break;
+
+            case '/wudc.html':
+            case '/wudc2018/es/campeonato.html':
+                $("#about").addClass('active');
+                break;
+
+            case '/hosts.html':
+            case 'es/anfitriones.html':
+                $("#about").addClass('active');
+                break;
+
+            case '/mexicocity.html':
+            case '/es/cdmx.html' :
+                $("#about").addClass('active');
+                break;
+
+            case '/registration.html':
+            case '/es/registro.html' :
+                $("#tournament").addClass('active');
+                break;
+
+            case ( '/payment.html' ) :
+                $("#tournament").addClass('active');
+                break;
+
+            case ( '/schedule.html' ) :
+                $("#tournament").addClass('active');
+                break;
+
+            case ( '/independent-adjudicators.html' ) :
+                $("#tournament").addClass('active');
+                break;
+
+            case ( '/masters-public.html' ) :
+                $("#tournament").addClass('active');
+                break;
+
+            case ( '/code-conduct.html' ) :
+                $("#tournament").addClass('active');
+                break;
+
+            case ( '/venues.html' ) :
+                $("#tournament").addClass('active');
+                break;
+
+            case ( '/visas.html' ) :
+                $("#tournament").addClass('active');
+                break;
+
+            case ( '/accomodation.html' ) :
+                $("#tournament").addClass('active');
+                break;
+
+            case ( '/volunteers.html' ) :
+                $("#tournament").addClass('active');
+                break;
+
+            case ( '/training-materials.html' ) :
+                $("#tournament").addClass('active');
+                break;
+
+            case ( '/faqs.html' ) :
+                $("#tournament").addClass('active');
+                break;
+
+            case '/press-releases.html':
+            case '/es/comunicados-prensa.html' :
+                $("#press").addClass('active');
+                break;
+
+            case '/articles-interviews.html':
+            case '/es/articulos-entrevistas.html' :
+                $("#press").addClass('active');
+                break;
+
+            case '/gallery.html':
+            case '/es/galeria.html' :
+                $("#press").addClass('active');
+                break;
+
+            case '/contact.html':
+            case '/es/contacto.html' :
+                $("#contact").addClass('active');
+                break;
+
+            default  :
+                break;
+        }
 
 
 	});
