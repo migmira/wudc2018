8ff0320cfb71277ec6d621ea08ca7ca81d9194d6
Miguel Windows <migmira@hotmail.com>
2017-07-02 15:09:15 -0500

    selected 2

--- js/active.js
+++ js/active.js
@@ -18,15 +18,18 @@
             case '/adjudication-core.html' :
             case '/es/adjudicacion.html':
                 $("#whoweare").addClass('active');
+                $("#whoweare-adj").addClass('selected');
                 break;
 
             case '/organizing-comittee.html':
             case '/es/comiteorganizador.html':
                 $("#whoweare").addClass('active');
+                $("#whoweare-org").addClass('selected');
                 break;
 
             case ( '/tab-team.html' ) :
                 $("#whoweare").addClass('active');
+                $("#whoweare-tab").addClass('selected');
                 break;
 
             case '/wudc.html':
@@ -50,65 +53,80 @@
             case '/registration.html':
             case '/es/registro.html' :
                 $("#tournament").addClass('active');
+                $("#tournament-registration").addClass('selected');
                 break;
 
             case ( '/payment.html' ) :
                 $("#tournament").addClass('active');
+                $("#tournament-payment").addClass('selected');
                 break;
 
             case ( '/schedule.html' ) :
                 $("#tournament").addClass('active');
+                $("#tournament-schedule").addClass('selected');
                 break;
 
             case ( '/independent-adjudicators.html' ) :
                 $("#tournament").addClass('active');
+                $("#tournament-indepen").addClass('selected');
                 break;
 
             case ( '/masters-public.html' ) :
                 $("#tournament").addClass('active');
+                $("#tournament-masters").addClass('selected');
                 break;
 
             case ( '/code-conduct.html' ) :
                 $("#tournament").addClass('active');
+                $("#tournament-conduct").addClass('selected');
                 break;
 
             case ( '/venues.html' ) :
                 $("#tournament").addClass('active');
+                $("#tournament-venues").addClass('selected');
                 break;
 
             case ( '/visas.html' ) :
                 $("#tournament").addClass('active');
+                $("#tournament-visas").addClass('selected');
                 break;
 
             case ( '/accomodation.html' ) :
                 $("#tournament").addClass('active');
+                $("#tournament-accom").addClass('selected');
                 break;
 
             case ( '/volunteers.html' ) :
                 $("#tournament").addClass('active');
+                $("#tournament-volunteers").addClass('selected');
                 break;
 
             case ( '/training-materials.html' ) :
                 $("#tournament").addClass('active');
+                $("#tournament-training").addClass('selected');
                 break;
 
             case ( '/faqs.html' ) :
                 $("#tournament").addClass('active');
+                $("#tournament-faqs").addClass('selected');
                 break;
 
             case '/press-releases.html':
             case '/es/comunicados-prensa.html' :
                 $("#press").addClass('active');
+                $("#press-releases").addClass('selected');
                 break;
 
             case '/articles-interviews.html':
             case '/es/articulos-entrevistas.html' :
                 $("#press").addClass('active');
+                $("#press-articles").addClass('selected');
                 break;
 
             case '/gallery.html':
             case '/es/galeria.html' :
                 $("#press").addClass('active');
+                $("#press-gallery").addClass('selected');
                 break;
 
             case '/contact.html':
