126dda5040742cfd0607e8f7e65f11416aa53059
migmira <migmira@hotmail.com>
2017-07-02 06:34:25 +0000

    testing activ.js

--- js/active.js
+++ js/active.js
@@ -1,84 +1,106 @@
-// $( document ).ready(function() {
+$( document ).ready(function() {
 	var path = window.location.pathname;
 	console.log(path);
 
- //   $(document).on('DOMNodeInserted', function(e) {
+    $(document).on('DOMNodeInserted', function(e) {
 
- //        console.log(e);
+         console.log(e);
    
-	// 	switch( path ) {
-	// 		case '/adjudication-core.html':
-	// 			$("#whoweare").addClass('active');
+		switch( path ) {
+			case '/adjudication-core.html':
+				$("#whoweare").addClass('active');
+				break;
 				
-	// 		case '/organizing-comittee.html':
-	// 			$("#whoweare").addClass('active');
+			case '/organizing-comittee.html':
+				$("#whoweare").addClass('active');
+				break;
 
-	// 		case '/tab-team.html':
-	// 			$("#whoweare").addClass('active');
+			case '/tab-team.html':
+				$("#whoweare").addClass('active');
+				break;
 			
-	// 		case '/wudc.html':
-	// 			$("#about").addClass('active');
+			case '/wudc.html':
+				$("#about").addClass('active');
+				break;
 				
-	// 		case '/hosts.html':
-	// 			$("#about").addClass('active');
+			case '/hosts.html':
+				$("#about").addClass('active');
+				break;
 				
-	// 		case '/mexicocity.html':
-	// 			$("#about").addClass('active');
+			case '/mexicocity.html':
+				$("#about").addClass('active');
+				break;
 				
-	// 		case '/registration.html':
-	// 			$("#tournament").addClass('active');
+			case '/registration.html':
+				$("#tournament").addClass('active');
+				break;
 				
-	// 		case '/payment.html':
-	// 			$("#tournament").addClass('active');
+			case '/payment.html':
+				$("#tournament").addClass('active');
+				break;
 				
-	// 		case '/schedule.html':
-	// 			$("#tournament").addClass('active');
+			case '/schedule.html':
+				$("#tournament").addClass('active');
+				break;
 				
-	// 		case '/independent-adjudicators.html':
-	// 			$("#tournament").addClass('active');
+			case '/independent-adjudicators.html':
+				$("#tournament").addClass('active');
+				break;
 				
-	// 		case '/masters-public.html':
-	// 			$("#tournament").addClass('active');
+			case '/masters-public.html':
+				$("#tournament").addClass('active');
+				break;
 				
-	// 		case '/code-conduct.html':
-	// 			$("#tournament").addClass('active');
+			case '/code-conduct.html':
+				$("#tournament").addClass('active');
+				break;
 				
-	// 		case '/venues.html':
-	// 			$("#tournament").addClass('active');
+			case '/venues.html':
+				$("#tournament").addClass('active');
+				break;
 				
-	// 		case '/visas.html':
-	// 			$("#tournament").addClass('active');
+			case '/visas.html':
+				$("#tournament").addClass('active');
+				break;
 				
-	// 		case '/accomodation.html':
-	// 			$("#tournament").addClass('active');
+			case '/accomodation.html':
+				$("#tournament").addClass('active');
+				break;
 				
-	// 		case '/volunteers.html':
-	// 			$("#tournament").addClass('active');
+			case '/volunteers.html':
+				$("#tournament").addClass('active');
+				break;
 				
-	// 		case '/training-materials.html':
-	// 			$("#tournament").addClass('active');
+			case '/training-materials.html':
+				$("#tournament").addClass('active');
+				break;
 				
-	// 		case '/faqs.html':
-	// 			$("#tournament").addClass('active');	
+			case '/faqs.html':
+				$("#tournament").addClass('active');
+				break;
 
-	// 		case '/press-releases.html':
-	// 			$("#partners").addClass('active');
+			case '/press-releases.html':
+				$("#partners").addClass('active');
+				break;
 				
-	// 		case '/articles-interviewss.html':
-	// 			$("#partners").addClass('active');
+			case '/articles-interviewss.html':
+				$("#partners").addClass('active');
+				break;
 				
-	// 		case '/gallery.html':
-	// 			$("#partners").addClass('active');
+			case '/gallery.html':
+				$("#partners").addClass('active');
+				break;
 
-	// 		case '/contact.html':
-	// 			$("#contact").addClass('active');		
+			case '/contact.html':
+				$("#contact").addClass('active');	
+				break;
 				
-	// 		default:
-	// 			break;
-	// 	}
+			default:
+				break;
+		}
 
 
 
-	// });
+	});
 	
-// });
+});
